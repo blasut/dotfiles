3cce6be9eedf6c7261fe3afbcbd9a511ba4f7685
oklaridentitet <oklaridentitet@gmail.com>
2015-05-03 18:15:53 +0200

    move config to seperate file

---
+++ emacs/.emacs.d/settings/key-bindings.el
@@ -0,0 +1,10 @@
+; This is to be able to execute commands without reaching for the alt key
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+
+(provide 'key-bindings)
