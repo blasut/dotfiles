74c42e10c6bca0eef5a1daabc452106ebef01f98
oklaridentitet <oklaridentitet@gmail.com>
2015-05-03 22:28:34 +0200

    use helm more with projectile

--- emacs/.emacs.d/settings/setup-projectile.el
+++ emacs/.emacs.d/settings/setup-projectile.el
@@ -4,4 +4,9 @@
 
 (require 'helm-projectile)
 
+(setq projectile-completion-system 'helm)
+(helm-projectile-on)
+
+(setq projectile-switch-project-action 'helm-projectile)
+
 (provide 'setup-projectile)
