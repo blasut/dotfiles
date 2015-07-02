85c6500c63a24ca7162d77bd9e2193d25d357a3e
oklaridentitet <oklaridentitet@gmail.com>
2015-07-02 17:55:21 +0200

    fix slime lol utf lol

--- emacs/.emacs.d/settings/misc.el
+++ emacs/.emacs.d/settings/misc.el
@@ -1,4 +1,6 @@
-(setq initial-scratch-message "; This but a scratch
+(setq initial-scratch-message ";; This but a scratch
 ")
 
+(set-language-environment "UTF-8")
+(setq slime-net-coding-system 'utf-8-unix)
 (provide 'misc)
