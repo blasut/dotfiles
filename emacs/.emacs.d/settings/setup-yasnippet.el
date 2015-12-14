538b32b3788d8659d615f9f98de402ac956d8007
blasut <blasut937@gmail.com>
2015-12-14 22:48:10 +0100

    use shift-tab with yasnippet

--- emacs/.emacs.d/settings/setup-yasnippet.el
+++ emacs/.emacs.d/settings/setup-yasnippet.el
@@ -3,4 +3,11 @@
 
 (yas-global-mode 1)
 
+;; Remove Yasnippet's default tab key binding
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+;; Set Yasnippet's key binding to shift+tab
+(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
+
+
 (provide 'setup-yasnippet)
