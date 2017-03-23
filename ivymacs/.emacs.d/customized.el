4ff8127ab5d27667a1118c5d4102db52d95491e4
blasut <blasut937@gmail.com>
2017-03-23 21:52:14 +0100

    add highlight-mode to base layer

--- ivymacs/.emacs.d/customized.el
+++ ivymacs/.emacs.d/customized.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" default)))
+    ("f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" default)))
  '(package-selected-packages
    (quote
-    (eval-in-repl exec-path-from-shell cider smooth-scrolling c-eldoc visual-regexp keyfreq restclient projectile hydra counsel swiper ace-window evil yasnippet flycheck parinfer evil-magit evil-surround slime htmlize org-babel-eval-in-repl which-key use-package rainbow-delimiters org-plus-contrib ob-restclient material-theme magit lispy general evil-escape dashboard counsel-projectile color-theme-solarized))))
+    (highlight-symbol color-theme-monokai monokai-theme eval-in-repl exec-path-from-shell cider smooth-scrolling c-eldoc visual-regexp keyfreq restclient projectile hydra counsel swiper ace-window evil yasnippet flycheck parinfer evil-magit evil-surround slime htmlize org-babel-eval-in-repl which-key use-package rainbow-delimiters org-plus-contrib ob-restclient material-theme magit lispy general evil-escape dashboard counsel-projectile color-theme-solarized))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
