008800e1fdefd43bf3004a41fa1d731d33ad4d23
blasut <blasut937@gmail.com>
2017-06-14 08:14:12 +0200

    parinfer for all

--- ivymacs/.emacs.d/customized.el
+++ ivymacs/.emacs.d/customized.el
@@ -8,7 +8,7 @@
     ("f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" default)))
  '(package-selected-packages
    (quote
-    (golden-ratio persp-mode winum highlight-symbol color-theme-monokai monokai-theme eval-in-repl exec-path-from-shell cider smooth-scrolling c-eldoc visual-regexp keyfreq restclient projectile hydra counsel swiper ace-window evil yasnippet flycheck parinfer evil-magit evil-surround slime htmlize org-babel-eval-in-repl which-key use-package rainbow-delimiters org-plus-contrib ob-restclient material-theme magit lispy general evil-escape dashboard counsel-projectile color-theme-solarized)))
+    (smartparens golden-ratio persp-mode winum highlight-symbol color-theme-monokai monokai-theme eval-in-repl exec-path-from-shell cider smooth-scrolling c-eldoc visual-regexp keyfreq restclient projectile hydra counsel swiper ace-window evil yasnippet flycheck parinfer evil-magit evil-surround slime htmlize org-babel-eval-in-repl which-key use-package rainbow-delimiters org-plus-contrib ob-restclient material-theme magit lispy general evil-escape dashboard counsel-projectile color-theme-solarized)))
  '(persp-mode t nil (persp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
