7ee7d7e41a0bfc9fafee54b76622640932c36ee1
blasut <blasut937@gmail.com>
2017-11-17 15:14:30 +0100

    install bash 4

--- setup/brew.sh
+++ setup/brew.sh
@@ -27,16 +27,16 @@ brew install gnu-sed --with-default-names
 # Bash 4                                                                      #
 ###############################################################################
 
-#  # Install Bash 4.
-#  brew install bash
-#  brew tap homebrew/versions
-#  brew install bash-completion2
-#  # We installed the new shell, now we have to activate it
-#  echo "Adding the newly installed shell to the list of allowed shells"
-#  # Prompts for password
-#  sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-#  # Change to the new shell, prompts for password
-#  chsh -s /usr/local/bin/bash
+# Install Bash 4.
+brew install bash
+# brew tap homebrew/versions
+brew install bash-completion2
+# We installed the new shell, now we have to activate it
+echo "Adding the newly installed shell to the list of allowed shells"
+# Prompts for password
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+# Change to the new shell, prompts for password
+chsh -s /usr/local/bin/bash
 
 ###############################################################################
 # Stuff I want                                                                #
