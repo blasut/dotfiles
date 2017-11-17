31b1a73d4d5f26cafad2b31a11cc65ce1d3b1400
blasut <blasut937@gmail.com>
2017-11-17 23:30:04 +0100

    add kibit and eastwood

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,2 +1,4 @@
-{:user {:plugins [[lein-pprint "1.1.1"]]}}
+{:user {:plugins [[lein-pprint "1.1.1"]
+                  [lein-kibit "0.1.5"]
+                  [jonase/eastwood "0.2.5"]]}}
 {:repl {:dependencies [^:displace [org.clojure/clojure "1.9.0-RC1"]]}}
