691a5f9aa3bdc2475005003e6cc6a5c266b5145f
blasut <blasut937@gmail.com>
2020-05-11 10:10:52 +0200

    add install sh wip for installing all deps needed

---
+++ doomemacs/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+## To install emacs with native json compilation
+# brew install emacs-plus --with-emacs-27-branch --with-jansson --HEAD
+
+## To install required LSPs
+
+## elixir ls (https://github.com/elixir-lsp/elixir-ls)
+# install requried erlang and elixir version then follow the compliation instructions in the readme
+
+## javascript https://github.com/sourcegraph/javascript-typescript-langserver.git
+# npm i -g javascript-typescript-langserver
