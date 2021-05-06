2f7364b7ba6affae34f842542bc2167d56000219
blasut <blasut937@gmail.com>
2021-05-06 09:14:19 +0200

    move more config into settings.org

--- doomemacs/.doom.d/config.el
+++ doomemacs/.doom.d/config.el
@@ -20,28 +20,9 @@
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Mono" :size 16))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-palenight)
+;; Try using babel org config with doom
+(org-babel-load-file
+   (expand-file-name "~/.doom.d/settings.org"))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -212,11 +193,6 @@
 
      )    )
   )
-
-;; Try using babel org config with doom
-(org-babel-load-file
-   (expand-file-name "~/.doom.d/settings.org"))
-
 ;; Let's try tabnine
                                         ;(use-package! company-tabnine
                                         ;  :after company
