822d519992ad25e04dd94f76743754fbd1d7ba4d
blasut <blasut937@gmail.com>
2021-05-24 09:32:21 +0200

    suppres before safe hook warning

--- doomemacs/.doom.d/custom.el
+++ doomemacs/.doom.d/custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(run-command-recipes
    '(run-command-recipe-local run-command-recipe-package-json run-command-recipe-mix))
- '(run-command-run-method 'compile))
+ '(run-command-run-method 'compile)
+ '(warning-suppress-types '((before-save-hook))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
