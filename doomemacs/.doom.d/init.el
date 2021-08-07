8d01acbe5028f786ffc59bef6fea5f0315bca150
blasut <blasut937@gmail.com>
2021-08-07 16:55:17 +0200

    add common lisp

--- doomemacs/.doom.d/init.el
+++ doomemacs/.doom.d/init.el
@@ -118,7 +118,7 @@
        (cc +lsp)           ; C > C++ == 1
        ;;beancount         ; mind the GAAP
        ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
