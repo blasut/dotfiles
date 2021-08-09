ea1d76fbc1fc68421f303f2f84bf1ef0f61d122f
christopher <christopher@fishbrain.com>
2021-08-09 11:02:45 +0200

    make sure solargraph works

--- doomemacs/.doom.d/custom.el
+++ doomemacs/.doom.d/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(lsp-solargraph-use-bundler t)
  '(run-command-recipes
    '(run-command-recipe-local run-command-recipe-package-json run-command-recipe-mix))
  '(run-command-run-method 'compile)
--- doomemacs/.doom.d/init.el
+++ doomemacs/.doom.d/init.el
@@ -163,7 +163,7 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails +lsp)  ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       (ruby +rails +lsp +rbenv +rails)  ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
