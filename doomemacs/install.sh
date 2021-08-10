ba71379ca4c7885764762f4be0ae0d932f7d3b6f
christopher <christopher@fishbrain.com>
2021-08-10 09:17:14 +0200

    update setup script

--- doomemacs/install.sh
+++ doomemacs/install.sh
@@ -1,12 +1,48 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+## TODO: Make this the "real" setup script
+
+brew install --cask firefox
+brew tap homebrew/cask-versions
+brew install --cask firefox-developer-edition
+brew install --cask iterm2
+brew install starship
+brew install --cask Slack
+brew install vim
+
+brew install git
+
 ## To install emacs with native json compilation
-# brew install stow
-# brew install emacs-plus
-# brew install ripgrep
-# brew install fd
-# brew install coreutils
+xcode-select --install
+brew tap d12frosted/emacs-plus
+brew install emacs-plus@28 --with-native-comp
+brew install stow
+brew install ripgrep
+brew install fd
+brew install coreutils
+
+# upgrade bash
+brew install bash
+
+# fonts
+brew install --cask font-hack
+brew install --cask  font-fira-code   font-fira-mono   font-fira-sans
+
+# databases
+# brew install mysql
+
+# aws stuff
+brew install --cask aws-vault
+brew install awscli
+brew install chamber
+
+# misc
+brew install --cask spotify
+brew install --cask karabiner-elements
+cd ~/dotfiles && stow karabiner && cd
+brew install --cask gpg-suite
+brew install --cask shiftit
 
 ## To install required LSPs
 
@@ -14,7 +50,13 @@ set -euo pipefail
 # install requried erlang and elixir version then follow the compliation instructions in the readme
 
 ## javascript https://github.com/sourcegraph/javascript-typescript-langserver.git
-# npm i -g javascript-typescript-langserver
+brew install nvm
+brew install yarn
+npm i -g javascript-typescript-langserver
+
+# ruby
+brew install rbenv
 
 # Trying out funky for local shell scripts
-#  pip install pyfunky
+brew install python
+pip3 install pyfunky
