a31f9210ed15831ca7ea123cd6e708f7424a6ef0
blasut <blasut937@gmail.com>
2022-06-01 09:51:04 +0200

    WIP org capture templates

--- doomemacs/.doom.d/config.el
+++ doomemacs/.doom.d/config.el
@@ -26,7 +26,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Dropbox/notes/work")
+(setq org-directory "~/Documents/notes")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
