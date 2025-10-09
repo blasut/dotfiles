29a4b4fa9c89b669f7aa6b17baace1f913b78f32
blasut <blasut937@gmail.com>
2025-10-09 13:32:53 +0200

    Cleanish slate for ivymacs

--- ivymacs/.emacs.d/init.el
+++ ivymacs/.emacs.d/init.el
@@ -2,10 +2,8 @@
 (setq package-enable-at-startup nil) ; tells emacs not to load any packages before starting up
 ;; the following lines tell emacs where on the internet to look up
 ;; for new packages.
-(setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("melpa"     . "https://melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 ;; Bootstrap `use-package'
@@ -15,10 +13,26 @@
 
 (eval-when-compile (require 'cl))
 
+;; Put this BEFORE (package-initialize) in your init.el
+(setq package-install-upgrade-built-in t)  ; Emacs 29+
+
 (use-package org
-  :ensure org-plus-contrib
+  :ensure t
+  :pin gnu  ; or :pin melpa if you want bleeding edge
   :config
   (progn
-    ;; config stuff
     (org-babel-load-file
      (expand-file-name "settings.org" user-emacs-directory))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org-plus-contrib)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
