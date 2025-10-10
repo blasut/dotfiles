514788c18074929f3564f70c063c1f0b1e5a792f
blasut <blasut937@gmail.com>
2025-10-10 15:51:58 +0200

    projectile is nice

--- ivymacs/.emacs.d/customized.el
+++ ivymacs/.emacs.d/customized.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" default))
- '(package-selected-packages '(org-plus-contrib)))
+ '(package-selected-packages '(counsel-projectile projectile org-plus-contrib)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
