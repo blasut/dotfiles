fd260e6cba9d4239e3f4fc7e53bcf0eb19d959cf
blasut <blasut937@gmail.com>
2025-10-10 14:33:14 +0200

    Make org mode look nicer

--- ivymacs/.emacs.d/customized.el
+++ ivymacs/.emacs.d/customized.el
@@ -11,4 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#FAFAFA" :foreground "#212121" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Source Code Pro")))))
+ )
