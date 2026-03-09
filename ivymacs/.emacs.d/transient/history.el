38b00ce4dc730dc2f352a7fdb687555ca9fc26b0
blasut <blasut937@gmail.com>
2026-03-09 19:36:26 +0100

    add persp mode

---
+++ ivymacs/.emacs.d/transient/history.el
@@ -0,0 +1,9 @@
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-fetch nil)
+ (magit-push nil
+             ("--force-with-lease"))
+ (magit-rebase
+  ("--autostash"))
+ (magit-status-jump nil)
+ (magit-submodule nil))
